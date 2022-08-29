8d36b9aef1b0979e77268f983df042ebc78b33d2
dackattack99 <zcdavies99@gmail.com>
2022-08-29 04:51:00 -0400

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    let newString = "";
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+    else {
+        for (i=1; i<=numTimes; i++) {
+            newString = newString + string;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
