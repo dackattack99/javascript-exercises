28671ec1b422adb8c93fca306e8a29a9632e92b7
dackattack99 <zcdavies99@gmail.com>
2022-08-29 05:22:33 -0400

    next

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let newString = "";
+  let charGet = "";
+  for (let i=string.length-1; i>=0; i--) {
+    charGet = string.charAt(i);
+    newString += charGet;
+  }
+  return newString;
 
-};
+
+}
+
+console.log(reverseString("Hello"));
+
 
 // Do not edit below this line
 module.exports = reverseString;
